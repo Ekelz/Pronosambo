cb5a2a99fcd47ec4e28de49fc52a4d0900e0dd12
Etienne <etiennedumontet@hotmail.com>
2019-10-08 16:31:00 +0200

    Ajout header

--- views/head.php
+++ views/head.php
@@ -1,24 +1,47 @@
 <?php
-ob_start();
+ob_start();²
 ?>
-<nav class="breadcrumb" aria-label="breadcrumbs">
-        <ul>
-            <li><a href="./home.php">Accueil</a></li>
-            <li><a href="#">Sources</a></li>
-        </ul>
-        <div class="navbar-end">
-            <div class="navbar-item">
-                <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Inscription</strong>
-                    </a>
-                    <form method="post" action="./home.php">
-                        <input class="button is-light" type="submit" name="btnLogin" value="Connexion">
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark sambo-background">
+        <img class=navbar-brand style="width:50px;" src="../src/img/logopronosambo.png">
+        <a class="navbar-brand" href="#">PRONOSAMBO</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Futures rencontres </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Résultats </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Combattants </a>
+                </li>
+                <li class="nav-item padding-left">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2 sambo-navbar-input sambo-background" type="search" placeholder="Rechercher un combattant..." aria-label="Search">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Recherche</button>
                     </form>
-                </div>
-            </div>
+                </li>
+            </ul>
+            <form>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">S'inscrire</a>
+                    </li>
+                    <li class="nav-item">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Se connecter</button>
+                    </li>
+                </ul>
+            </form>
         </div>
     </nav>
+</header>
 <?php
-$head=ob_get_clean();
+$head = ob_get_clean();
 ?>
